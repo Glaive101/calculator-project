8f41051da4c65617dd97fb330cd7f96c7ad26049
Paul van Niekerk <stormwindpro1@gmail.com>
2026-01-14 14:18:23 +0200

    More project configuration settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,25 @@
 import js from "@eslint/js";
+import airbnbBase from "eslint-config-airbnb-base";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  js.configs.recommended,
+  airbnbBase,
+  {
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } 
+  },
+  { files: ["**/*.js"], 
+    languageOptions: { sourceType: "commonjs" } 
+  },
+  {
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "no-debugger": "error",
+    },
+  }
 ]);
